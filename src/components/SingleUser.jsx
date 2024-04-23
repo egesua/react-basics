b6a7fe36cd0216d004df35faadb72f8f534ccbe6
egesua <egesu06@gmail.com>
2024-04-23 14:08:59 +0300

    singleUser component wanted informations fetched.

--- src/components/SingleUser.jsx
+++ src/components/SingleUser.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
-const SingleUser = () => {
+const SingleUser = ({ user }) => {
   return (
-    <div>
-      SingleUser
+    <div className="single-user">
+      <img src={user.picture.large} alt="user-pictures" />
+      <div className="user-info">
+        <h3>
+          {user.name.first} {user.name.last}
+        </h3>
+        <p className="user-mail">Email: {user.email}</p>
+        <p className="user-location">Location: {user.location.country} | {user.location.city}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default SingleUser;
