5d0cbffff3f1ede4928943af08e6a0cb125aa2be
egesua <egesu06@gmail.com>
2024-04-23 14:00:18 +0300

    mapping and created singleUser component.

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -5,17 +5,25 @@ const UserList = () => {
   const [users, setUsers] = useState([]);
 
   const fetchUsers = async () => {
-    const response = await axios.get("https://randomuser.me/api/?results=5")
-    setUsers(response.data.results)
-  }
+    const response = await axios.get("https://randomuser.me/api/?results=5");
+    setUsers(response.data.results);
+  };
 
   useEffect(() => {
     fetchUsers();
-  },[]);
+  }, []);
 
-  return <div>
-    Hi.
-  </div>;
+  return(
+    <div className="user-list">
+      <ul>
+        {users.map((user, idx) => {(
+          <li>
+            <SingleUser user={user} key={idx}/>
+          </li>
+        )})}
+      </ul>
+    </div>
+  ) 
 };
 
 export default UserList;
